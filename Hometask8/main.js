794aab5cf1fc2d86381a7b54299c986244a9f97d
kalinasasha30 <112653857+kalinasasha30@users.noreply.github.com>
2022-10-28 21:06:52 +0300

    Add files via upload

---
+++ Hometask8/main.js
@@ -0,0 +1,58 @@
+// 1
+
+// let mas1 = ['a', 'b', 'c'] 
+// let mas2 = [1, 2, 3]
+// let mas3 = mas1 + mas2
+// let mas = mas3.replaceAll(',','')
+// console.log(mas.split(''))
+
+// 2
+
+// let mas = ['a', 'b', 'c'] 
+// let a = mas.push(1,2,3)
+// console.log(mas)
+
+// 3
+
+// let mas = [1, 2, 3]
+// let a = mas.push(4,5,6)
+// console.log(mas)
+
+// 4
+
+// let mas = [1, 2, 3]
+// let a = mas.unshift(4,5,6)
+// console.log(mas)
+
+// 5
+
+// let mas = ['js', 'css', 'jq']
+// console.log(mas[0])
+
+// 6
+
+// let mas = ['js', 'css', 'jq']
+// console.log(mas[mas.length - 1])
+
+// 7
+
+// let obj = {
+//     js:'test', 
+//     jq: 'hello', 
+//     css: 'world'
+// }
+// console.log(Object.keys(obj));
+
+// 8
+
+// let mas = [1, 2, 3, 4, 5]
+// let a = mas.splice(3,0, 'a', 'b', 'c')
+// console.log(mas)
+
+// 9
+
+// let mas = [1, 2, 3, 4, 5]
+// let a = mas.splice(1,0, 'a', 'b')
+// let b = mas.splice(6,0, 'c')
+// let c = mas.splice(8,0, 'e')
+// console.log(mas)
